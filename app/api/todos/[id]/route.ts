18b1a78ce5970e30e9a9830f1db3b4bd03f88476
Ilkin Ibadov <ilkin.ibadov21@gmail.com>
2026-01-30 11:06:26 +0400

    id routes added

---
+++ app/api/todos/[id]/route.ts
@@ -0,0 +1,51 @@
+import { createClient } from "@/lib/supabase/server";
+
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+    const supabase = await createClient();
+    const id = params.id
+
+    if (!id) return new Response(JSON.stringify({ error: 'ID is required' }), { status: 400 });
+
+    const { data, error } = await supabase.from('todos').select('*').eq('id', id).single();
+
+    if (error) return new Response(JSON.stringify({ error: 'Blog not found' }), { status: 404 });
+
+    return new Response(JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
+
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
+    const supabase = await createClient();
+    const body = await req.json();
+    const id = params.id;
+    const { title, description, completed } = body;
+
+    if (!id) return new Response(JSON.stringify({ error: 'ID is required' }), { status: 400 });
+
+    const { data, error } = await supabase
+        .from('todos')
+        .update({ title, description, completed })
+        .eq('id', id)
+        .single();
+
+    if (error) return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+
+    return new Response(JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    const supabase = await createClient()
+
+    const id = Number(params.id);
+
+    if (!id) return new Response(JSON.stringify({ error: 'ID is required' }), { status: 400 });
+
+    const { error } = await supabase.from('todos').delete().eq('id', id);
+
+    if (error) return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+
+    return new Response(JSON.stringify({ message: 'Todo successfully deleted' }), { status: 204 });
+}
--- app/protected/todos/page.tsx
+++ app/protected/todos/page.tsx
@@ -20,15 +20,37 @@ const Todos = () => {
     }
   }
 
+  const deleteTodo = async (id: number) => {
+    try {
+      const res = await fetch(`/api/todos/${id}`, {
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+
+      if (res.ok) {
+        const data = await res.json()
+        console.log(data.message)
+        // alert(data.message)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   useEffect(() => {
     fetchTodos()
   }, [])
 
   return (
     <div className="w-full h-screen grid grid-cols-4 gap-5 p-5">
-      {todos.map(todo => <div className="border border-zinc-300 p-5">
+      {todos.map(todo => <div className="border border-zinc-300 p-5 h-fit">
         <h3>{todo.title}</h3>
         <p>{todo.description}</p>
+        <button onClick={() => {
+          deleteTodo(todo.id)
+        }}>Delete</button>
       </div>)}
     </div>
   )
